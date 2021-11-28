56404e1a66a965326e844287db412b0123283f38
Serhii-Khrapun <Sergiy.Khrapun@gmail.com>
2021-11-28 13:02:43 +0200

    оновив

--- src/js/fetch-images.js
+++ src/js/fetch-images.js
@@ -1,33 +1,31 @@
 import axios from 'axios';
 
 export default class ImagesApiService {
-    constructor() {
-        this.searchQuery = '';
-        this.page = 1;
-    }
+  constructor() {
+    this.searchQuery = '';
+    this.page = 1;
+  }
 
-   async fetchImages() {
-        const BASE_URL = 'https://pixabay.com';
-        const KEY = '24443838-ead6510e85d5ce0a082bd02f6';
-        const getImg = await axios.get(`${BASE_URL}/api/?key=${KEY}&q=${this.searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&page=${this.page}&per_page=40`)
-        this.incrementPage();
-        
-        return getImg;
-          }   
+  async fetchImages() {
+    const BASE_URL = 'https://pixabay.com';
+    const KEY = '24443838-ead6510e85d5ce0a082bd02f6';
+    let getImg = await axios.get(
+      `${BASE_URL}/api/?key=${KEY}&q=${this.searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&page=${this.page}&per_page=40`,
+    );
+    this.incrementPage();
 
+    return getImg;
+  }
 
-    resetPage() {
-        this.page = 1;
-    }
+  resetPage() {
+    this.page = 1;
+  }
 
-    incrementPage() {
-        this.page += 1;
-    }
+  incrementPage() {
+    this.page += 1;
+  }
 
-    decrementPage() {
-        this.page -= 1;
-    }
-    
-  
-
+  decrementPage() {
+    this.page -= 1;
+  }
 }
--- src/js/image-search.js
+++ src/js/image-search.js
@@ -4,75 +4,64 @@ import ImagesApiService from './fetch-images';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const refs = {
-    searchBtn: document.querySelector('.search-form > button'),
-    searchForm: document.querySelector('.search-form'),
-    imagesContainer: document.querySelector('.gallery'),
-    loadMoreBtn: document.querySelector('.load-more'),
-    body: document.querySelector('body'),
-}
+  searchBtn: document.querySelector('.search-form > button'),
+  searchForm: document.querySelector('.search-form'),
+  imagesContainer: document.querySelector('.gallery'),
+  loadMoreBtn: document.querySelector('.load-more'),
+  body: document.querySelector('body'),
+};
 const imagesApiService = new ImagesApiService();
 
 refs.searchForm.addEventListener('submit', onSearch);
 refs.loadMoreBtn.addEventListener('click', onLoadMore);
 
 async function onSearch(e) {
-    e.preventDefault();
-    imagesApiService.searchQuery = e.currentTarget.elements.searchQuery.value;
+  e.preventDefault();
+  imagesApiService.searchQuery = e.currentTarget.elements.searchQuery.value;
 
-const good = await imagesApiService.fetchImages();
-const totalHits =  good.data.totalHits;  
-const hitsLength = good.data.hits.length;  
-  
-        if(totalHits < 1) {
-            Notify.failure("Sorry, there are no images matching your search query. Please try again.");
-            return;
-        }
-    else if(imagesApiService.searchQuery.trim() === '') {
-        Notify.warning('Enter your serch query, please :)');
-        return;
-    }
-    else {
-      refs.body.style.background = "#ffffff"
+  const good = await imagesApiService.fetchImages();
+  const totalHits = good.data.totalHits;
+  const hitsLength = good.data.hits.length;
+
+  if (totalHits < 1) {
+    Notify.failure('Sorry, there are no images matching your search query. Please try again.');
+    return;
+  } else if (imagesApiService.searchQuery.trim() === '') {
+    Notify.warning('Enter your serch query, please :)');
+    return;
+  } else {
+    refs.body.style.background = '#ffffff';
     Notify.success(`Hooray! We found ${totalHits} images.`);
-    refs.imagesContainer.innerHTML = ''; 
-    
+    refs.imagesContainer.innerHTML = '';
+
     imagesApiService.resetPage();
-   await imagesApiService.fetchImages().then(renderPosts);
-    
-   
-    
-    
-    
+    await imagesApiService.fetchImages().then(renderPosts);
+
     refs.loadMoreBtn.classList.remove('is-hidden');
     refs.loadMoreBtn.removeAttribute('disabled');
-     if (hitsLength < 40) {
-       refs.loadMoreBtn.classList.add('is-hidden');
-        Notify.info("We're sorry, but you've reached the end of search results.");
-       return;
-    }
-    
-   
-    }
-    }
-
-;
-
-async function onLoadMore() {
-    
-    if (imagesApiService.page  !== 2) {
-        imagesApiService.decrementPage();
-    };
-    
-    await imagesApiService.fetchImages().then(renderPosts);
-    const good = await imagesApiService.fetchImages(); 
-    const hitsLength = good.data.hits.length; 
-   
     if (hitsLength < 40) {
       refs.loadMoreBtn.classList.add('is-hidden');
-       Notify.info("We're sorry, but you've reached the end of search results.");
+      Notify.info("We're sorry, but you've reached the end of search results.");
+      return;
     }
+  }
+}
 
-    const { height: cardHeight } = document
+async function onLoadMore() {
+  if (imagesApiService.page !== 2) {
+    imagesApiService.decrementPage();
+  }
+
+  await imagesApiService.fetchImages().then(renderPosts);
+  const good = await imagesApiService.fetchImages();
+  const hitsLength = good.data.hits.length;
+
+  if (hitsLength < 40) {
+    refs.loadMoreBtn.classList.add('is-hidden');
+    Notify.info("We're sorry, but you've reached the end of search results.");
+  }
+
+  const { height: cardHeight } = document
     .querySelector('.gallery')
     .firstElementChild.getBoundingClientRect();
 
@@ -80,14 +69,12 @@ async function onLoadMore() {
     top: cardHeight * 2 + 180,
     behavior: 'smooth',
   });
-
 }
 
- function renderPosts(getImg) {
-        const markup = 
-        getImg.data.hits
-    .map(({webformatURL,largeImageURL,tags,likes,views,comments,downloads}) => {
-        return `<div class="photo-card">
+function renderPosts(getImg) {
+  const markup = getImg.data.hits
+    .map(({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) => {
+      return `<div class="photo-card">
         <a class="gallery__link" href="${largeImageURL}">
             <img class="gallery__image" src="${webformatURL}" alt="${tags}" loading="lazy" width="392" height="264"/>
             </a>
@@ -106,11 +93,10 @@ async function onLoadMore() {
               </p>
             </div>
           </div>`;
-          })
-          .join("");
-          refs.imagesContainer.insertAdjacentHTML('beforeend', markup);
-          let gallery = new SimpleLightbox('.gallery a');
-          gallery.refresh();
-          refs.loadMoreBtn.classList.remove('is-hidden');
-    }
-    
+    })
+    .join('');
+  refs.imagesContainer.insertAdjacentHTML('beforeend', markup);
+  let gallery = new SimpleLightbox('.gallery a');
+  gallery.refresh();
+  refs.loadMoreBtn.classList.remove('is-hidden');
+}
